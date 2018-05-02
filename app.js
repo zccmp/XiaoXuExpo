53ed1892645d3f3ea53b75234cede834f8907ce0
sasonhe <642139009@qq.com>
2018-05-02 18:24:25 +0800

    注释

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //app.js
 App({
-  onLaunch: function () {
+  onLaunch: function() {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    // 登录
+    // 登录登录登录登录登录登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
