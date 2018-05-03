a2a54ff2e075cb4a39ab4698ba2c81b0ab7c6376
zccmp <zccmp@163.com>
2018-05-03 10:47:38 +0800

    Update app.js
    更新app.js

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-//123
-    // 登录登录登录登录登录登录
+    // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
