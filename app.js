34802acef9484fd440bdcebde6e7946138ba6ca5
sasonhe <642139009@qq.com>
2018-05-03 09:36:12 +0800

    信息

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//app.js
+﻿//app.js
 App({
   onLaunch: function() {
     // 展示本地存储能力
@@ -12,8 +12,7 @@ App({
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
-    // 获取用户信息
-    wx.getSetting({
+    // 获取用户信息信息信息信息信息    wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
